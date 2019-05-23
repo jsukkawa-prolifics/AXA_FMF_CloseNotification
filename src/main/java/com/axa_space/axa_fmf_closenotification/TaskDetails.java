8cc7173db0fad1a67bfdfe4cb5e3eb58ffb8288d
pamAdmin <>
2019-05-23 14:02:27 -0400

    {/src/main/java/com/axa_space/axa_fmf_closenotification/TaskDetails.java}

--- src/main/java/com/axa_space/axa_fmf_closenotification/TaskDetails.java
+++ src/main/java/com/axa_space/axa_fmf_closenotification/TaskDetails.java
@@ -8,22 +8,7 @@ public class TaskDetails implements java.io.Serializable {
 
 	static final long serialVersionUID = 1L;
 
-	@org.kie.api.definition.type.Label(value = "Comments")
-	private java.util.List<java.lang.String> comments;
-
 	public TaskDetails() {
-	}
-
-	public java.util.List<java.lang.String> getComments() {
-		return this.comments;
-	}
-
-	public void setComments(java.util.List<java.lang.String> comments) {
-		this.comments = comments;
-	}
-
-	public TaskDetails(java.util.List<java.lang.String> comments) {
-		this.comments = comments;
 	}
 
 }
